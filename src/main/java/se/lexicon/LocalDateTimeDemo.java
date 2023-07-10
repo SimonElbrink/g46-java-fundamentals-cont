8c8327529c981f2245adf850be6569ec53767039
Simon Elbrink <simon.elbrink@lexicon.se>
2023-07-10 11:50:02 +0200

    LocalDateTimeDemo

---
+++ src/main/java/se/lexicon/LocalDateTimeDemo.java
@@ -0,0 +1,23 @@
+package se.lexicon;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeDemo {
+    public static void main(String[] args) {
+
+        LocalDateTime timestamp = LocalDateTime.now();
+        LocalDateTime nextWorkshop = LocalDateTime.parse("2023-07-12T09:00");
+        LocalDateTime endOfWorkDay = LocalDateTime.of(2023,7, 10,17,0);
+
+//        timestamp.plus();
+
+        // https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+        System.out.println("timestamp = " + timestamp);
+        System.out.println("timestamp = " + timestamp.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        System.out.println("timestamp = " + timestamp.format(DateTimeFormatter.ISO_WEEK_DATE));
+        System.out.println("timestamp = " + timestamp.format(DateTimeFormatter.ofPattern("eeee dd MM y 'T' hh:mm:ss")));
+
+
+    }
+}
