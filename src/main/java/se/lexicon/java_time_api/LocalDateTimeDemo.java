0e5ef103f1ec27e27151954daf7419bc9406f26a
Simon Elbrink <simon.elbrink@lexicon.se>
2023-07-10 13:53:13 +0200

    Reorganize folder structure

--- src/main/java/se/lexicon/java_time_api/LocalDateTimeDemo.java
+++ src/main/java/se/lexicon/java_time_api/LocalDateTimeDemo.java
@@ -1,4 +1,4 @@
-package se.lexicon;
+package se.lexicon.java_time_api;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
--- src/main/java/se/lexicon/static_instance/Product.java
+++ src/main/java/se/lexicon/static_instance/Product.java
@@ -1,4 +1,4 @@
-package se.lexicon;
+package se.lexicon.static_instance;
 
 public class Product {
 
