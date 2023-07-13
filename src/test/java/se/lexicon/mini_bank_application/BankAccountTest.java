63b782e830a527181386a2de1544fc72e7dbc903
Simon Elbrink <simon.elbrink@lexicon.se>
2023-07-13 12:00:46 +0200

    Mini Bank Application - Implemented Deposit Test

--- src/test/java/se/lexicon/mini_bank_application/BankAccountTest.java
+++ src/test/java/se/lexicon/mini_bank_application/BankAccountTest.java
@@ -2,6 +2,7 @@ package se.lexicon.mini_bank_application;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 class BankAccountTest {
 
 
@@ -16,12 +17,41 @@ class BankAccountTest {
     }
 
     @Test
-    void deposit_should_succeed_when_amount_positive(){
+    void deposit_should_succeed_when_amount_positive() {
+
+        BankAccount account = new BankAccount();
+
+        //Balance = 0
+
+        account.deposit(500);
+
+        //Balance 500
+
+
+        double expectedBalance = 500;
+        double actualBalance = account.balance;
+
+//        if (account.balance == 500){
+//            System.out.println("CORRECT!");
+//        }else {
+//            System.out.println("Balance is not correct!");
+//        }
+
+        assertEquals(expectedBalance, actualBalance, "Balance should be updated to 500");
 
     }
 
     @Test
-    void deposit_should_fail_when_amount_negative(){
+    void deposit_should_fail_when_amount_negative() {
+        BankAccount account = new BankAccount();
+
+        boolean deposit = account.deposit(-650);
+
+        double expectedBalance = 0;
+        double actualBalance = account.balance;
+
+        assertEquals(expectedBalance, actualBalance);
+        assertFalse(deposit);
 
     }
 
