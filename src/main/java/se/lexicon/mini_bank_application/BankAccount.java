111d57486b3d8571f1539623f4b116df2a6c8fbf
Simon Elbrink <simon.elbrink@lexicon.se>
2023-07-13 10:55:25 +0200

    Mini Bank Application - Start

---
+++ src/main/java/se/lexicon/mini_bank_application/BankAccount.java
@@ -0,0 +1,20 @@
+package se.lexicon.mini_bank_application;
+
+public class BankAccount {
+
+    String accountIdentification; // 902 556 588 664
+    double balance;
+    String ownerName;
+
+    public double withdraw(double amount){
+        return 0;
+    }
+
+    public boolean deposit(double amount){
+        return false;
+    }
+
+
+
+
+}
---
+++ src/main/java/se/lexicon/mini_bank_application/Main.java
@@ -0,0 +1,9 @@
+package se.lexicon.mini_bank_application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
