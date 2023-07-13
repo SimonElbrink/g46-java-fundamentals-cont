2f6c07f2eca11091306239686170ab57fb61b0da
Simon Elbrink <simon.elbrink@lexicon.se>
2023-07-13 10:56:22 +0200

    Mini Bank Application - Implemented Test Scenarios Studs

---
+++ src/test/java/se/lexicon/mini_bank_application/BankAccountTest.java
@@ -0,0 +1,30 @@
+package se.lexicon.mini_bank_application;
+
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+
+
+    @Test
+    void withdraw_should_fail_when_amount_higher_then_balance(){
+
+    }
+
+    @Test
+    void withdraw_should_fail_when_amount_is_negative(){
+
+    }
+
+    @Test
+    void deposit_should_succeed_when_amount_positive(){
+
+    }
+
+    @Test
+    void deposit_should_fail_when_amount_negative(){
+
+    }
+
+
+
+}
